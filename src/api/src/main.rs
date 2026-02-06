12ddec7a4d8d5d35f5dc38455397ddd1dc7360f0
aea555 <akarahmet2002@gmail.com>
2026-02-06 04:10:28 +0300

    minor fix

--- src/api/src/main.rs
+++ src/api/src/main.rs
@@ -79,7 +79,7 @@ async fn main() -> anyhow::Result<()> {
     tracing::info!("Server listening on {}", addr);
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
-    axum::serve(listener, app).await?;
+    axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await?;
 
     Ok(())
 }
